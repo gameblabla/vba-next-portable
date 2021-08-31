be08e8db5ecbebb13934a1c7930a9fde7ebe1c7b
gameblabla <gameblabla@protonmail.com>
2021-08-31 19:47:42 +0200

    Fix Tinyalsa backend. (it needs the other parameters to work)

--- shell/audio/tinyalsa/sound_output.cpp
+++ shell/audio/tinyalsa/sound_output.cpp
@@ -8,35 +8,39 @@
 #include "sound_output.h"
 struct pcm *pcm_out;
 
+#define CARD_DEFAULT 0
+
 uint32_t Audio_Init()
 {
-	struct pcm_config config;
-	
-    config.channels = 2;
-    config.rate = SOUND_OUTPUT_FREQUENCY;
+#ifdef NONBLOCKING_AUDIO
+	int flags = PCM_OUT | PCM_NONBLOCK | PCM_MMAP;
+#else
+	int flags = PCM_OUT | PCM_MMAP;
+#endif
+    struct pcm_config config = {
+        .channels = 2,
+        .rate = SOUND_OUTPUT_FREQUENCY,
+        .format = PCM_FORMAT_S16_LE
+    };
+    
     config.period_size = SOUND_SAMPLES_SIZE;
-    config.period_count = SOUND_SAMPLES_SIZE / 512;
-	config.format = PCM_FORMAT_S16_LE;
-	
-    pcm_out = pcm_open(0, 0, PCM_OUT, &config);
+    config.period_count = 2;
+    config.start_threshold = config.period_size;
+    config.silence_threshold = config.period_size * 2;
+    config.stop_threshold = config.period_size * 2;
+    
+    pcm_out = pcm_open(CARD_DEFAULT, 0, flags, &config);
     if (!pcm_out)
     {
-        printf("failed to allocate memory for PCM\n");
-        return -1;
-    }
-    else if (!pcm_is_ready(pcm_out))
-    {
-        pcm_close(pcm_out);
-        printf("failed to open PCM\n");
-        return -1;
-    }
-    
+		printf("Does not exit\n");
+		return 0;
+	}
+	
 	return 1;
 }
 
 void Audio_Write(int16_t* buffer, uint32_t buffer_size)
 {
-	if (pcm_out != NULL) return;
 	pcm_writei(pcm_out, buffer, buffer_size);
 }
 
